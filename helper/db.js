144d4b6a20e723961272032492ee101a63bb1cd1
BatuhanYerinde <batuyerinde@gmail.com>
2019-12-01 21:06:53 +0300

    movies ve directors endPointlerinin yazılması.

---
+++ helper/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+  mongoose.connect('mongodb://filmler:batu5206@ds057538.mlab.com:57538/heroku_kjqbrc99', { useNewUrlParser: true, useUnifiedTopology: true  });
+
+  mongoose.connection.on('open', () => {
+     console.log('MongoDB: Connected');
+  });
+  mongoose.connection.on('error', (err) => {
+      console.log('MongoDB Connection Error:',err);
+  });
+  mongoose.Promise = global.Promise;
+};
